454de904b3c120b91a58b62d739501a571369430
Lucas Ledesma <lucasledesma27@gmail.com>
2022-01-26 11:44:28 -0300

    clase 3, se agrego navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import './Navbar.js';
+import Navbar from './Navbar.js';
 
 function App() {
   return (
     <div className="App">
+        <Navbar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
