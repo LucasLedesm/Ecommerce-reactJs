947fd486c2cdde622a61ac323cecf32cb8c54aeb
Lucas Ledesma <lucasledesma27@gmail.com>
2022-02-15 18:52:49 -0300

    bug avanzado

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,29 +1,24 @@
 import React from 'react'
 import ItemCount from './ItemCount';
-import Itemlist from './ItemList'
 
-const Item = ({item}) => {
-    Itemlist();
-
-    
+const Item = ({id,title,stock,img,price}) => {
   return (
     <div className="container">
-
         <div className="col">
             <div className="card shadow-sm">
-                <h1>{item.title}</h1>
-                <img src={item.img} alt="ilustracion"  /> 
+                <h1>{title}</h1>
+                <img src={img} alt="ilustracion"  /> 
                 <div className="card-body">
-                    <p className="card-text">{item.description}</p>
+                    <p className="card-text">description</p>
                     <div className="d-flex justify-content-center align-items-center">
                         <div className="btn-group">
-                            <ItemCount stock={item.stock} initial={1} />
+                            <ItemCount stock={stock} initial={1} />
                         </div>
                     </div>
                 </div>
             </div>
             <div>
-                <h3>${item.price} Stock:{item.stock}</h3>
+                <h3>${price} Stock:{stock}</h3>
             </div>
         </div>
     </div>
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,18 +1,77 @@
-import React from 'react';
-import Item from "./Item";
+import React, { useState } from 'react';
+import ItemList from "./ItemList"
+
+
+
+const productosJson = [
+    {
+        id: 1,
+        title: "rouser ns200",
+        stock: 32,
+        img: "src: https://cdn.bajajauto.com/-/media/Images/bajajauto/brands/bajaj-bikes/pulsar/pulsar-ns-200/design/webp/Design-01.ashx",
+        price: 332000
+    },
+    {
+        id: 2,
+        title: "kawazaki Z400",
+        stock: 2,
+        img: "src: https://cdn.bajajauto.com/-/media/Images/bajajauto/brands/bajaj-bikes/pulsar/pulsar-ns-200/design/webp/Design-01.ashx",
+        price: 747000
+    },
+    {
+        id: 3,
+        title: "Honda CBR600",
+        stock: 6,
+        img: "src: https://cdn.bajajauto.com/-/media/Images/bajajauto/brands/bajaj-bikes/pulsar/pulsar-ns-200/design/webp/Design-01.ashx",
+        price: 942000
+    },
+    {
+        id: 4,
+        title: "Yamaha R1",
+        stock: 5,
+        img: "src: https://cdn.bajajauto.com/-/media/Images/bajajauto/brands/bajaj-bikes/pulsar/pulsar-ns-200/design/webp/Design-01.ashx",
+        price: 1332000
+    },
+    {
+        id: 5,
+        title: "Susuki gpx1000",
+        stock: 1,
+        img: "src: https://cdn.bajajauto.com/-/media/Images/bajajauto/brands/bajaj-bikes/pulsar/pulsar-ns-200/design/webp/Design-01.ashx",
+        price: 1232000
+    },
+]
 
 const ItemListContainer = () => {
+    const [ItemList, setItemList] = useState([])
+    const prodPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            setItemList(productosJson)
+            ItemList ? resolve(ItemList) : reject("no se completo")
+        }, 2000);
+    })
+
+
+    prodPromise
+        .then(console.log)
+        .catch(console.warn)
     return <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-        <Item/>
-        <Item/>
-        <Item/>
-        <Item/>
-        <Item/>
-        <Item/>
-        <Item/>
-        <Item/>
-        <Item/>
+        <ItemList ItemList={ItemList} />
+
     </div>
 };
+
+
+// const [productos, setproductos] = useState([])
+// const prodPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         setproductos(productosJson)
+//         productos ? resolve(productos) : reject("no se completo")
+//     }, 2000);
+// })
+
+
+// prodPromise
+//     .then(console.log)
+//     .catch(console.warn)
 
 export default ItemListContainer;
